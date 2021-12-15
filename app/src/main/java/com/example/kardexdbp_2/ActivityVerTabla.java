337b767c5d8faffda49cefdc02031e7cd34648f7
Justo Perez <justo.perez@ucsp.edu.pe>
2021-12-15 18:32:55 -0500

    Todo funciona segunda rama

--- app/src/main/java/com/example/kardexdbp_2/ActivityVerTabla.java
+++ app/src/main/java/com/example/kardexdbp_2/ActivityVerTabla.java
@@ -24,13 +24,17 @@ public class ActivityVerTabla extends AppCompatActivity {
     private TableLayout tableLayout;
     private String[]header = {"Mes/Dia","Detalle","Entrada","Salida","Precio"};
 
-    private ArrayList<String[]> rows = new ArrayList<>();
+    static public ArrayList<String[]> rows = new ArrayList<>();
     public FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public String[][][]añadir = new String[12][31][4];
+
+
+
     public String[]meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-    //public String[][]meses = {{"Enero"}, {"Febrero"}, {"Marzo"}, {"Abril"}, {"Mayo"}, {"Junio"}, {"Julio"}, {"Agosto"}, {"Septiembre"}, {"Octubre"}, {"Noviembre"}, {"Diciembre"}};
     private TableDynamic tableDynamic;
 
+
+
+    static public String[][][]añadir = new String[12][31][4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,23 +46,68 @@ public class ActivityVerTabla extends AppCompatActivity {
         tableDynamic = new TableDynamic(tableLayout, getApplicationContext());
 
 
+
+        for (int i = 0 ; i < 12; i++)
+        {
+            for ( int j = 1; j < 32 ; j++)
+            {
+                DocumentReference docRef = db.collection(meses[i]).document(String.valueOf(j));
+                int finalJ = j;
+                int finalI = i;
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            añadir[finalI][finalJ -1][0] = ""+document.get("Detalle");
+                            añadir[finalI][finalJ -1][1] = ""+document.get("Entrada");
+                            añadir[finalI][finalJ -1][2] = ""+document.get("Precio");
+                            añadir[finalI][finalJ -1][3] = ""+document.get("Salida");
+                            Log.d("Wait", "Cargando archivos!!!");
+                        }
+                    }
+                });
+
+            }
+        }
         tableDynamic.addHeader(header);
+    }
+    public void save(View view)
+    {
+
         tableDynamic.addData(getClients());
 
-    }
 
+
+    }
 
     private ArrayList<String[]> getClients()
     {
-        for (int i = 0; i < meses.length; i++)
+        int existencias_actuales = 0,total_ventas= 0,total_rebastecimiento= 0;
+
+        for (int i = 0; i< 12; i++)
         {
-            rows.add(new String[]{meses[i], "Pedro", "Lopez"});
+            for (int j = 1; j <32;j++)
+            {
+                if (añadir[i][j-1][0] != null )
+                {
+                    rows.add(new String[]{meses[i]+"/"+(j), añadir[i][j-1][0],añadir[i][j-1][1],añadir[i][j-1][3],añadir[i][j-1][2] });
+                    existencias_actuales = existencias_actuales +   Integer.parseInt(añadir[i][j-1][1]) - Integer.parseInt(añadir[i][j-1][3]);
+                    total_ventas = total_ventas + Integer.parseInt(añadir[i][j-1][3]);
+                    total_rebastecimiento = total_rebastecimiento + Integer.parseInt(añadir[i][j-1][1]);
+                }
+            }
         }
 
-        rows.add(new String[]{"4", "Fiorella", "Canto"});
+        rows.add(new String[]{"          ", "          ","          ","          ","          "});
+        rows.add(new String[]{"Existencias actuales", "Total de ventas","Total de Rebastecimientos","          ","          "});
+        rows.add(new String[]{String.valueOf(existencias_actuales), String.valueOf(total_ventas),String.valueOf(total_rebastecimiento),"",""});
+
         return rows;
     }
-    //        rows.add(new String[]{"list.get(i)", "a"});
+
+
+
     public void CambiarActivity(View view){
         //Cambio de Activity
         Intent i = new Intent(this, InicioActivity.class);
